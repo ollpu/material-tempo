36cb713ce14eb13df026405e47bcb478ac6299ec
Roope Salmi <rpsalmi@gmail.com>
2015-03-22 21:28:21 +0200

    Some work on FavoriteBPM class

--- app/src/main/java/fi/zah/ollpu/materialtempo/FavoriteBPM.java
+++ app/src/main/java/fi/zah/ollpu/materialtempo/FavoriteBPM.java
@@ -1,7 +1,12 @@
 package fi.zah.ollpu.materialtempo;
 
+import android.content.Context;
+
 /**
  * Created by ollpu on 22.3.2015.
+ * int simple: A simplification of the original BPM, used as key in SharedPreferences
+ *  - Also used to match similar BPMs into one
+ * float accurate: The original BPM
  */
 public class FavoriteBPM {
     int simple;
@@ -15,6 +20,16 @@ public class FavoriteBPM {
 
     FavoriteBPM(float accurate) {
         this.accurate = accurate;
-        simple = Math.round((accurate - 2.5f) / 5) * 5;
+        simple = simplify(accurate);
+    }
+
+    private int simplify(float original) {
+        return Math.round((original - 2.5f) / 5) * 5;
+    }
+
+
+    public static FavoriteBPM findMatch(float of, Context context) {
+        int ofSimple = simplify(of);
+
     }
 }
