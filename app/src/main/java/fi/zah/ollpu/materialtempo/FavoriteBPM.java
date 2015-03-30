583b9ee771c7c0449e1438801f05aa3937a2bc27
Roope Salmi <rpsalmi@gmail.com>
2015-03-30 23:16:49 +0300

    Avoid overwriting existing fav when editing a fav The user will now be queried before overwriting an existing favorite. Missing:  - Translation  - Refresh favs from sharedaPref (currently works a bit tackily)   - Optionally just properly reflect changes made to sharedPref in the ListView

--- app/src/main/java/fi/zah/ollpu/materialtempo/FavoriteBPM.java
+++ app/src/main/java/fi/zah/ollpu/materialtempo/FavoriteBPM.java
@@ -61,7 +61,12 @@ public class FavoriteBPM {
             + " BPM";
     }
 
-    public void edit(Context context, final FavoritesAdapter adapter, final SharedPreferences sharedPref) {
+    public void edit(final Context context, final FavoritesAdapter adapter, final SharedPreferences sharedPref) {
+        editPreset(context, adapter, sharedPref, name, String.format("%.2f", accurate).replace(",", "."));
+
+    }
+
+    private void editPreset(final Context context, final FavoritesAdapter adapter, final SharedPreferences sharedPref, String nm, String vl) {
         Resources res = context.getResources();
 
         LayoutInflater layoutInflater = LayoutInflater.from(context);
@@ -71,9 +76,9 @@ public class FavoriteBPM {
         alertDialogBuilder.setView(promptView);
 
         final EditText inputName = (EditText) promptView.findViewById(R.id.editName);
-        inputName.setText(name);
+        inputName.setText(nm);
         final EditText inputBPM = (EditText) promptView.findViewById(R.id.editBPM);
-        inputBPM.setText(String.format("%.2f", accurate).replace(",", "."));
+        inputBPM.setText(vl);
 
         alertDialogBuilder
                 .setTitle(res.getString(R.string.edit_fav))
@@ -81,19 +86,18 @@ public class FavoriteBPM {
                 .setPositiveButton(res.getString(R.string.ok),
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-
                                 SharedPreferences.Editor editor = sharedPref.edit();
                                 editor.remove(name);
-                                name = inputName.getText().toString();
-                                editor.putFloat(name, reassignBPM(Float.parseFloat(inputBPM.getText().toString())));
-                                editor.apply();
-
-                                adapter.notifyDataSetChanged();
+                                String newName = inputName.getText().toString();
+                                String valueText = inputBPM.getText().toString();
+                                if (!sharedPref.contains(newName)) {
+                                    submitEdit(newName, valueText, editor, adapter);
+                                } else edit_sameName(context, adapter, sharedPref, newName, valueText);
                             }
                         })
                 .setNegativeButton(res.getString(R.string.cancel),
                         new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,	int id) {
+                            public void onClick(DialogInterface dialog, int id) {
                                 dialog.cancel();
                             }
                         });
@@ -101,6 +105,54 @@ public class FavoriteBPM {
         AlertDialog alertD = alertDialogBuilder.create();
 
         alertD.show();
+    }
 
+
+    private int edit_sameName(final Context context, final FavoritesAdapter adapter, final SharedPreferences sharedPref, final String newName, final String valueText) {
+        final Resources res = context.getResources();
+
+        final FavoriteBPM me = this;
+
+        final int[] toReturn = new int[1];
+        toReturn[0] = 0;
+        new AlertDialog.Builder(context)
+                .setTitle(res.getText(R.string.favEdit_overwrite_title))
+                .setMessage(res.getText(R.string.favEdit_overwrite_message))
+                .setPositiveButton(res.getString(R.string.ok),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {edit_sameNameCallback(2, context, adapter, sharedPref, newName, valueText);
+                            }
+                        })
+                .setNegativeButton(res.getString(R.string.cancel),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {edit_sameNameCallback(0, context, adapter, sharedPref, newName, valueText);
+                            }
+                        })
+                .setNeutralButton(res.getString(R.string.action_edit),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {edit_sameNameCallback(1, context, adapter, sharedPref, newName, valueText);
+                            }
+                        })
+                .show();
+        return toReturn[0];
+    }
+
+
+    private void edit_sameNameCallback(int result, Context context, FavoritesAdapter adapter, SharedPreferences sharedPref, String newName, String valueText) {
+        SharedPreferences.Editor editor = sharedPref.edit();
+        switch(result) {
+            case 1:
+                editPreset(context, adapter, sharedPref, newName, valueText);
+            case 2:
+                submitEdit(newName, valueText, editor, adapter);
+        }
+    }
+
+    private void submitEdit(String newName, String valueText, SharedPreferences.Editor editor, FavoritesAdapter adapter) {
+        name = newName;
+        editor.putFloat(name, reassignBPM(Float.parseFloat(valueText)));
+        editor.apply();
+
+        adapter.notifyDataSetChanged();
     }
 }
